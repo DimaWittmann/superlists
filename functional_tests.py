a82770e73aa36244ce71aee5ec67e68480d587bf
DimaWittmann <dimawittmann@gmail.com>
2015-05-29 13:47:00 +0300

    Funtional test now checks, we can inpot a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -16,32 +17,48 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get('http://localhost:8000')
 
 		print(self.browser.title)
-		self.asserIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		self.assertIn('To-Do', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
-
+		
 
 		# She is invited to enter a to-do item straight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+			)
 
 
+		# She types "Buy peackock feathers" int a text box (Edith's hobby
+		# is tying fly-fishing lures)
+		inputbox.send_keys('Buy peacock feathers')
+
+		# When she hits enter, the page udpates, and now the page lists
+		# "1: Buy peackock feather's" as an item in a to-do list
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
+		# THere is still a text box inviting her to add another item. She
+		# She enters "Use peacock feathers to make a fly" (Edith is very methodical)
+		self.fail('Finish the test!')
+
+		# The page update again, and now shows both item on her list
+
+		# Edith wonders whether the site will remember her list. Then she sees
+		# that the site has generated a unique ULR for her -- there is same
+		# explanatory text to that effect.
+
+		# She visits that URL - her to-do list is still there.
+
+		# Satisfied, she goes back to sleep
+		
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
-# She types "Buy peackock feathers" int a text box (Edith's hobby
-# is tying fly-fishing lures)
 
-# When she hits enter, the page udpates, and now the page lists
-# "1: Buy peackock feather's" as an item in a to-do list
-
-# THere is still a text box inviting her to add another item. She
-# She enters "Use peacock feathers to make a fly" (Edith is very methodical)
-
-# The page update again, and now shows both item on her list
-
-# Edith wonders whether the site will remember her list. Then she sees
-# that the site has generated a unique ULR for her -- there is same
-# explanatory text to that effect.
-
-# She visits that URL - her to-do list is still there.
-
-# Satisfied, she goes back to sleep
 
