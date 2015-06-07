47321f248fc70e59ba3429b0b4b90f43840b43fc
DimaWittmann <dimawittmann@gmail.com>
2015-06-07 12:07:00 +0300

    Hack FT runner to be able to test stagin

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,13 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
-
+		self.browser.refresh()
+		
 	def tearDown(self):
 		self.browser.refresh()
 		self.browser.quit()
@@ -22,7 +24,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_can_start_a_list_and_retrive_it_later(self):
 
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 
 		print(self.browser.title)
 		self.assertIn('To-Do', self.browser.title)
@@ -68,7 +70,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		self.browser = webdriver.Firefox()
 
 		#Francis visits the home page. There is no sign of Edith's list
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -93,7 +95,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_layout_and_styling(self):
 		# Edith goes to the home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 
 		#She notices the input box is nicely centerd
@@ -112,4 +114,19 @@ class NewVisitorTest(StaticLiveServerTestCase):
 			512,
 			delta=5
 		)
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
+
 		
