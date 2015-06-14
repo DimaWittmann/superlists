ba3e9694160ea9d56f5f75bc404e921ce553aac6
DimaWittmann <dimawittmann@gmail.com>
2015-06-14 12:09:14 +0300

    Implement duplicate item validation at model layer

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -5,9 +5,9 @@ from lists.models import Item, List
 
 class ItemFormTest(TestCase):
 
-	def test_form_renders_item_text_input(self):
-		form = ItemForm()
-		self.fail(form.as_p())
+	#def test_form_renders_item_text_input(self):
+	#	form = ItemForm()
+	#	self.fail(form.as_p())
 
 
 	def test_form_item_input_has_placeholder_and_css_classes(self):
